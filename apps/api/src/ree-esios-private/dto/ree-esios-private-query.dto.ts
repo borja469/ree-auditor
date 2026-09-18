73918ab4c49d3b13d507ef6864c86c860b5d9c3f
Borja Añaños <Diroperaciones@oficina.local>
2026-09-18 18:25:05 +0200

    Preserve REE eSIOS query parameters

--- apps/api/src/ree-esios-private/dto/ree-esios-private-query.dto.ts
+++ apps/api/src/ree-esios-private/dto/ree-esios-private-query.dto.ts
@@ -1,10 +1,35 @@
+import { IsIn, IsOptional, IsString, Matches } from "class-validator";
+
 export class ReeEsiosPrivateMessagesQueryDto {
+  @IsOptional()
+  @Matches(/^\d{4}-\d{2}-\d{2}$/)
   date?: string;
+
+  @IsOptional()
+  @IsString()
   startTime?: string;
+
+  @IsOptional()
+  @IsString()
   endTime?: string;
+
+  @IsOptional()
+  @IsIn(["Application", "Server"])
   intervalType?: "Application" | "Server";
+
+  @IsOptional()
+  @IsString()
   messageType?: string;
+
+  @IsOptional()
+  @IsString()
   messageIdentification?: string;
+
+  @IsOptional()
+  @IsString()
   owner?: string;
+
+  @IsOptional()
+  @IsString()
   code?: string;
 }
